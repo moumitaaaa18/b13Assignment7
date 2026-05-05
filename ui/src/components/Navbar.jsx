808651db0b8324a2e434670492c7274aded7251b
moumitaaaa18 <dharchowdhurymoumita812@gmail.com>
2026-05-05 13:23:19 +0600

    navbar fixed

--- ui/src/components/Navbar.jsx
+++ ui/src/components/Navbar.jsx
@@ -2,53 +2,18 @@ import { NavLink } from "react-router-dom";
 
 function Navbar() {
   return (
-    <header style={styles.navbar}>
-      <div style={styles.logo}>
-  Keen<span style={styles.green}>Keeper</span>
-</div>
+    <nav className="navbar">
+      <h2 className="logo">
+        Keen<span>Keeper</span>
+      </h2>
 
-      <nav style={styles.links}>
-        <NavLink to="/" style={navStyle}>⌂ Home</NavLink>
-        <NavLink to="/timeline" style={navStyle}>◴ Timeline</NavLink>
-        <NavLink to="/stats" style={navStyle}>⌁ Stats</NavLink>
-      </nav>
-    </header>
+      <div className="nav-links">
+        <NavLink to="/">⌂ Home</NavLink>
+        <NavLink to="/timeline">◴ Timeline</NavLink>
+        <NavLink to="/stats">⌁ Stats</NavLink>
+      </div>
+    </nav>
   );
-}
-
-const navStyle = ({ isActive }) => ({
-  textDecoration: "none",
-  color: isActive ? "#fff" : "#6b7280",
-  background: isActive ? "#1f5c3f" : "transparent",
-  padding: "9px 14px",
-  borderRadius: "4px",
-  fontWeight: "700",
-  fontSize: "14px"
-});
-
-const styles = {
-  navbar: {
-    height: "70px",
-    background: "#fff",
-    borderBottom: "1px solid #e5e7eb",
-    display: "flex",
-    alignItems: "center",
-    justifyContent: "space-between",
-    padding: "0 70px"
-  },
-  logo: {
-    fontSize: "21px",
-    fontWeight: "800",
-    color: "#1f2937"
-  },
-  links: {
-    display: "flex",
-    alignItems: "center",
-    gap: "10px"
-  },
-  green: {
-  color: "#1f5c3f"
-  }
 }
 
 export default Navbar;
